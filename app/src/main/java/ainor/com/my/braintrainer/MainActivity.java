12845e85bd6799b11fa6e4e21581e0de2d5d6f33
Ainor Syahrizal <macintorch@gmail.com>
2016-12-20 18:24:38 +0800

    update sumTextView with random number

--- app/src/main/java/ainor/com/my/braintrainer/MainActivity.java
+++ app/src/main/java/ainor/com/my/braintrainer/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +22,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         startButton = (Button) findViewById(R.id.startButton);
+        TextView sumTextView = (TextView) findViewById(R.id.sumTextView);
+
+        Random rand = new Random();
+
+        //create a random number between 0-21
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        //update sumTextView text
+        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
     }
 }
